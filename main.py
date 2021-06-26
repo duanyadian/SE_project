e13b02918f0085fa1dafff81279b36e2d9530107
785288076@qq.com <785288076@qq.com>
2021-06-26 12:44:46 +0800

    用例执行

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from testcases.First_round_test.test_user_register import TestUserRegister
 from testcases.First_round_test.test_user_login import TestUserLogin
+from testcases.First_round_test.test_admin_login import TestAdminLogin
 
 if __name__ == '__main__':
+    # 执行test_user_register用例
     case = TestUserRegister()
     case.test_user_register_username()
     case.test_user_register_email()
@@ -9,10 +11,22 @@ if __name__ == '__main__':
     case.test_user_register_Confirmpwd()
     case.test_user_register_different()
     case.test_register_code_error()
+    case.test_user_register_OK()
 
+    # 执行test_user_login用例
     case1 = TestUserLogin()
     case1.test_user_login_nameEmpty()
     case1.test_user_login_nameError()
     case1.test_user_login_pwdEmpty()
     case1.test_user_login_pwdError()
     case1.test_user_login_OK()
+
+    # 执行test_admin_login用例
+    case2 = TestAdminLogin()
+    case2.test_admin_login_nameEmpty()
+    case2.test_admin_login_nameError()
+    case2.test_admin_login_pwdEmpty()
+    case2.test_admin_login_pwdError()
+    case2.test_admin_login_captchaEmpty()
+    case2.test_admin_login_captchaError()
+    case2.test_admin_login_OK()
