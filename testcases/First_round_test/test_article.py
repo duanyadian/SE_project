10b038442c82e82b7d412c58f28977a4890bd739
785288076@qq.com <785288076@qq.com>
2021-07-01 15:38:08 +0800

    测试用例

--- testcases/First_round_test/test_article.py
+++ testcases/First_round_test/test_article.py
@@ -2,7 +2,7 @@ from time import sleep
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.wait import WebDriverWait # 显性等待
 
 class TestArticle(object):
     def __init__(self,login):
